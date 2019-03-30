35bdd5fe109110f97024e4d41ca26536f1825514
Andy <ajonheck@gmail.com>
2019-03-30 18:15:42 -0500

    Fixes power calculation
    Tests and fixes power calculation. Currently unable to use _smac intrinsic for unknown reasons.

--- TSK_calculate_pwr.c
+++ TSK_calculate_pwr.c
@@ -11,34 +11,37 @@
 #include <mbx.h>
 #include "c55x.h"
 
+extern MBX_Obj MBX_TSK_calculate_pwr_data_in;
+
+int16_t pwr(const int16_t *l_data, const int16_t *r_data, const int16_t len);
+
 void tsk_calculate_pwr(Arg value_arg)
 {
 	AudioFrame_t frame1, frame2;
-	int40_t accum;
-	int16_t i, ave;
-
+	int16_t power;
 	while(1)
 	{
+		frame1.channel = RIGHT;
+		frame2.channel = LEFT;
 		// wait for first frame
 		MBX_pend(&MBX_TSK_calculate_pwr_data_in, &frame1, ~0);
 
 		// wait for the second frame
 		MBX_pend(&MBX_TSK_calculate_pwr_data_in, &frame2, ~0);
 
-		// verify the two frames are different channels
-		if(frame1.channel != frame2.channel)
-		{
-			// calculate power of average of the two channels
-			accum = 0;
-			for(i = 0; i < LEN_AUDIO_FRAME; i ++)
-			{
-				// find average by dividing each sample by 2 then adding
-				ave = _sadd(_shrs(frame1.frame[i], 1), _shrs(frame2.frame[i], 1));
-				accum = _smac(accum, ave, ave);
-			}
-			accum = _lshrs(_rnd(accum),16);
-			accum /= LEN_AUDIO_FRAME;
-		}
+		power = pwr(frame2.frame, frame2.frame, LEN_AUDIO_FRAME);
 	}
+}
 
+int16_t pwr(const int16_t *l_data, const int16_t *r_data, const int16_t len)
+{
+	int40_t p = 0;
+	int16_t value, i;
+	for(i = 0; i < len; i ++)
+	{
+		value = _sadd(l_data[i] >> 1, r_data[i] >> 1);
+		p += _lsmpy(value, value);
+	}
+	p = _lshrs(_rnd(p / len),16);
+	return (int16_t) p;
 }
