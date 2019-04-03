ff304ac798336b7b8e3b9629d76bad50f4126814
Andy <ajonheck@gmail.com>
2019-04-02 22:40:03 -0500

    Adds multi-frame power averaging

--- TSK_calculate_pwr.c
+++ TSK_calculate_pwr.c
@@ -12,35 +12,42 @@
 #include <mbx.h>
 #include "c55x.h"
 
+#define NUM_SAMPLED_FRAMES 28
+
 extern MBX_Obj MBX_TSK_calculate_pwr_data_in;
 extern MBX_Obj MBX_IDL_disp_power_data_in;
 
-int16_t pwr(const int16_t *l_data, const int16_t *r_data, const int16_t len);
+int16_t pwr(const int16_t *data, const int16_t len);
 
 void tsk_calculate_pwr(Arg value_arg)
 {
-	AudioFrame_t f1, f2;
-	int16_t power;
+	AudioFrame_t fin;
+
+	int16_t power, i;
+	int40_t power_accum;
 	while(1)
 	{
-		// wait for first frame
-		MBX_pend(&MBX_TSK_calculate_pwr_data_in, &f1, ~0);
+		power_accum = 0;
+		for(i = 0; i < NUM_SAMPLED_FRAMES; i ++)
+		{
+			// calculate power for multiple frames
+			MBX_pend(&MBX_TSK_calculate_pwr_data_in, &fin, ~0);
+			power_accum += pwr(fin.frame, LEN_AUDIO_FRAME);
+		}
 
-		// wait for the second frame
-		MBX_pend(&MBX_TSK_calculate_pwr_data_in, &f2, ~0);
+		power = power_accum / NUM_SAMPLED_FRAMES;
 
-		power = pwr(f1.frame, f2.frame, LEN_AUDIO_FRAME);
 		MBX_post(&MBX_IDL_disp_power_data_in, &power, 0);
 	}
 }
 
-int16_t pwr(const int16_t *l_data, const int16_t *r_data, const int16_t len)
+int16_t pwr(const int16_t *data, const int16_t len)
 {
 	int40_t p = 0;
 	int16_t value, i;
 	for(i = 0; i < len; i ++)
 	{
-		value = _sadd(l_data[i] >> 1, r_data[i] >> 1);
+		value = data[i];
 		p += _lsmpy(value, value);
 	}
 	p = _lshrs(_rnd(p / len),16);
