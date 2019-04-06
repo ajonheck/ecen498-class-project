a136925c4317476d6271c9255c4f6a57cb616ea7
Andy <ajonheck@gmail.com>
2019-04-06 13:53:20 -0500

    Adds FFT

--- TSK_calculate_pwr.c
+++ TSK_calculate_pwr.c
@@ -12,15 +12,48 @@
 #include <mbx.h>
 #include "c55x.h"
 
+#include "fft/FFT.h"
+
 #define NUM_SAMPLED_FRAMES 28
 
 extern MBX_Obj MBX_TSK_calculate_pwr_data_in;
 extern MBX_Obj MBX_IDL_disp_power_data_in;
 
+extern ExtU_FFT_T FFT_U;
+extern ExtY_FFT_T FFT_Y;
+
 int16_t pwr(const int16_t *data, const int16_t len);
 
 void tsk_calculate_pwr(Arg value_arg)
 {
+	AudioFrame_t fin;
+	int16_t i;
+	int16_t u_i = 0;
+	int16_t out[256];
+	while(1)
+	{
+		MBX_pend(&MBX_TSK_calculate_pwr_data_in, &fin, ~0);
+		// collect only left audio
+		if(fin.channel == LEFT)
+		{
+			// Copy until end of input frame or full fft buffer
+			for(i = 0; i < LEN_AUDIO_FRAME && u_i < LEN_FFT_INPUT;)
+			{
+				FFT_U.In1[u_i] = fin.frame[i];
+				u_i ++;
+				i++;
+			}
+
+			// Check if buffer is full
+			if(u_i == LEN_FFT_INPUT)
+			{
+				u_i = 0;
+				FFT_step();
+				memcpy(out, FFT_Y.Out1, sizeof(int16_t) * LEN_FFT_OUTPUT);
+			}
+		}
+	}
+	/*
 	AudioFrame_t fin;
 
 	int16_t power, i;
@@ -39,6 +72,7 @@ void tsk_calculate_pwr(Arg value_arg)
 
 		MBX_post(&MBX_IDL_disp_power_data_in, &power, 0);
 	}
+	*/
 }
 
 int16_t pwr(const int16_t *data, const int16_t len)
